74cb1a5f45e89f535e9b40292734742f90433274
Chukwuemeka Emeche <j0she1ife31@gmail.com>
2025-06-09 14:23:59 -0700

    return number to string

---
+++ Codewars/8kyu/numberToString.js
@@ -0,0 +1,15 @@
+/*
+We need a function that can transform a number (integer) into a string.
+
+function numberToString(num) {
+  // Return a string of the number here!
+}
+*/
+
+// Answer:
+// To simply turn the number to a string, I just used the String() to convert
+function numberToString(num) {
+  return String(num);
+}
+
+numberToString(3);
