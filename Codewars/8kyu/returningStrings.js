21754788832514b1daa3664dd3c3b126f36f2522
Chukwuemeka Emeche <j0she1ife31@gmail.com>
2025-06-16 13:42:14 -0700

    return string

---
+++ Codewars/8kyu/returningStrings.js
@@ -0,0 +1,15 @@
+/*
+Create a function that accepts a parameter representing a name and 
+returns the message: "Hello, <name> how are you doing today?".
+
+function greet(name){
+  //your code here
+}
+*/
+
+// Answer
+// Using the string literal, I returned message while using the 
+// ${} to put the name put in the parameter.
+function greet(name){
+  return `Hello, ${name} how are you doing today?`
+}
