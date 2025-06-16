bb0cd333ef4affe2cb636f126306c42fd917c6b7
Chukwuemeka Emeche <j0she1ife31@gmail.com>
2025-06-15 18:21:34 -0700

    return hello world

---
+++ Codewars/8kyu/helloWorld.js
@@ -0,0 +1,12 @@
+/*
+Make a simple function called greet that returns the most-famous "hello world!".
+
+// Write a function "greet" that returns "hello world!"
+*/
+
+// Answer:
+// I just returned the string 'hello world' in the function
+function greet() {
+  return "hello world!";
+}
+
